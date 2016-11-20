05e02c731b52d246685f08bc95b8794d84eff69f
James <jamesizhou@gmail.com>
2016-11-19 16:31:22 -0800

    updated database

--- PlanMyTrip/app/src/main/java/com/planmytrip/johan/planmytrip/DatabaseOpenHelper.java
+++ PlanMyTrip/app/src/main/java/com/planmytrip/johan/planmytrip/DatabaseOpenHelper.java
@@ -9,8 +9,8 @@ import android.content.Context;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME = "vangtfs.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "newvangtfs.db";
+    private static final int DATABASE_VERSION = 2;
 
     public DatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
